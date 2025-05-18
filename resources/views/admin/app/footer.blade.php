20c551d2b9bda42be4e0630c594c1a14c2342b5e
Jonsnow118 <doanvietdung11804@gmail.com>
2025-05-18 20:04:56 +0700

    first commit

---
+++ resources/views/admin/app/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+             © 2025 Bản quyền thuộc về:
+             <a class="text-info" href="/ ">Bitis SHOP - Doan Viet Dung</a>
+        </div>
+    </div>
+ </footer>
+ 
---
+++ resources/views/app/gioithieu/gioithieu.blade.php
@@ -0,0 +1,50 @@
+<div class="card mb-3 shadow-5" style="background-color: #EEEEEE">
+    <div class="card-body" style="margin-top:40px" >
+        <center>
+            <h3 class="card-title">GIỚI THIỆU</h3>
+        </center>
+    </div>
+    <br>
+</div>
+
+<div class="container">
+    <br>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/trang-chu">Trang chủ</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Giới thiệu</li>
+        </ol>
+    </nav>
+    <br>
+
+    <div class="card mb-3" style="max-width: 100%;">
+        <div class="row g-0">
+            <div class="col-xl-3">
+                <img src="{{ URL('images1/gioithieu.jpg') }}" alt="..." class="img-fluid rounded" />
+            </div>
+
+            <div class="col-xl-9">
+                <div class="card-body">
+                    <h5 class="card-title">Bitis SHOP - WEBSITE BÁN GIÀY</h5>
+                    <br>
+                    <div class="card-text">
+                        <p>Tại Biti’s Shop, chúng tôi tự hào mang đến cho bạn bộ sưu tập giày dép chất lượng cao, phù hợp cho mọi thành viên trong gia đình – từ nam, nữ đến trẻ em. Với hơn 40 năm kinh nghiệm, Biti’s luôn không ngừng đổi mới để đáp ứng xu hướng thời trang hiện đại, đồng thời đảm bảo sự êm ái, bền bỉ và tiện lợi cho từng bước chân. </p>
+                        <p>Dù bạn tìm kiếm đôi giày thể thao năng động, dép sandal tiện dụng hay giày thời trang cá tính – Biti’s đều có những lựa chọn hoàn hảo dành cho bạn. Hãy cùng chúng tôi nâng niu đôi chân, đồng hành trên mọi hành trình với giá trị Việt – chất lượng quốc tế.</p>
+
+                    </div>
+                    <p class="card-text">
+                        <i><small class="text-success">Đăng ký tài khoản và bắt đầu mua sắm đôi giày mơ ước của bạn!</small></i>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <br>
+    <br>
+    <br>
+
+
+</div>
